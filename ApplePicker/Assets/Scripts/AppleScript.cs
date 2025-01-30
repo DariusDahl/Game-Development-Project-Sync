a9a0b7ea9a71deec431e8174a5f62e9e22f865a7
Darius <dariusmdahl@gmail.com>
2025-01-29 18:44:14 -0600

    Created ApplePickerScript, other various things

--- ApplePicker/Assets/Scripts/AppleScript.cs
+++ ApplePicker/Assets/Scripts/AppleScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ApplePickerScript : MonoBehaviour
+public class AppleScript : MonoBehaviour
 {
 
     public static float bottomY = -20;
